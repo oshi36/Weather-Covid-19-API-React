e36ed67f32f5dbedfa6c46c714bec23c52047246
Oshi Gupta <oshiagupta36@gmail.com>
2020-08-11 21:24:01 +0530

    Updates

---
+++ precache-manifest.f3141c35f72d82059403eac9da4247f8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585b0ff00f1520e7705c00a4328d04f8",
+    "url": "/Weather-Covid-19-API-React/index.html"
+  },
+  {
+    "revision": "38acb6eb3217dd7f1759",
+    "url": "/Weather-Covid-19-API-React/static/css/2.26420363.chunk.css"
+  },
+  {
+    "revision": "61781b6891cd23c87dad",
+    "url": "/Weather-Covid-19-API-React/static/css/main.ccb08182.chunk.css"
+  },
+  {
+    "revision": "38acb6eb3217dd7f1759",
+    "url": "/Weather-Covid-19-API-React/static/js/2.92a1cb07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Covid-19-API-React/static/js/2.92a1cb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61781b6891cd23c87dad",
+    "url": "/Weather-Covid-19-API-React/static/js/main.4b9c5b0e.chunk.js"
+  },
+  {
+    "revision": "268fd27e46e6ae254d30",
+    "url": "/Weather-Covid-19-API-React/static/js/runtime-main.2b966c7d.js"
+  },
+  {
+    "revision": "3dcb1c4237737a884eeea81d2c3fbe80",
+    "url": "/Weather-Covid-19-API-React/static/media/covid.3dcb1c42.jpg"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/Weather-Covid-19-API-React/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/Weather-Covid-19-API-React/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/Weather-Covid-19-API-React/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/Weather-Covid-19-API-React/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
