8a2285965e0977ea82dbb53131c7af5bba716768
oshi36 <47573417+oshi36@users.noreply.github.com>
2020-08-11 21:10:59 +0530

    Uploaded Successfully

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
+import './App.css';
+import Weather from './components/Weather';
+import Form from './components/Form';
+import Axios from 'axios';
+const APIKey = '4efd4ccbb998b1ae294a02560d8712e4';
+
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			city: undefined,
+			country: undefined,
+			icon: undefined,
+			celsius: undefined,
+			temp_max: undefined,
+			temp_min: undefined,
+			description: '',
+			error: false,
+			covid: {},
+			covidUpdateByCountry: {}
+		};
+
+		this.weatherIcon = {
+			Thuderstorm: 'wi-thunderstorm',
+			Drizzle: 'wi-sleet',
+			Rain: 'wi-storm-showers',
+			Snow: 'wi-snow',
+			Atmosphere: 'wi-fog',
+			Clear: 'wi-day-sunny',
+			Clouds: 'wi-day-fog'
+		};
+	}
+	calCelsius(temp) {
+		let celsius = Math.floor(temp - 273.15);
+		return celsius;
+	}
+	calcCelsius(temp) {
+		let celsius = temp - 273.15;
+		return celsius.toFixed(1);
+	}
+	getWeatherIcon(icons, rangeId) {
+		switch (true) {
+			case rangeId >= 200 && rangeId <= 232:
+				this.setState({ icon: icons.Thuderstorm });
+				break;
+			case rangeId >= 300 && rangeId <= 321:
+				this.setState({ icon: icons.Drizzle });
+				break;
+			case rangeId >= 500 && rangeId <= 531:
+				this.setState({ icon: icons.Rain });
+				break;
+			case rangeId >= 600 && rangeId <= 622:
+				this.setState({ icon: icons.Snow });
+				break;
+			case rangeId >= 701 && rangeId <= 781:
+				this.setState({ icon: icons.Atmosphere });
+				break;
+			case rangeId === 800:
+				this.setState({ icon: icons.Clear });
+				break;
+			case rangeId >= 801 && rangeId <= 804:
+				this.setState({ icon: icons.Clouds });
+				break;
+			default:
+				this.setState({ icon: icons.Clouds });
+				break;
+		}
+	}
+	componentDidMount() {
+		Axios.get(`https://api.covid19api.com/summary`).then((response) => {
+			this.setState({
+				covid: response.data.Global
+			});
+		
+		});
+	}
+	getCovid() {
+		Axios.get(`https://api.covid19api.com/dayone/country/${this.state.country}`).then((response) => {
+			this.setState({
+				covidUpdateByCountry: response.data[response.data.length - 1]
+			});
+				});
+	}
+	getWeather = async (event) => {
+		event.preventDefault();
+		let e = event.target;
+		let city = event.target.elements.city.value;
+		let country = event.target.elements.country.value;
+
+		if (city && country) {
+			const apiCall = await fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APIKey}`
+			);
+			const response = await apiCall.json();
+		
+			this.setState({
+				city: `${response.name},${response.sys.country}`,
+				country: response.sys.country,
+				celsius: this.calCelsius(response.main.temp),
+				temp_max: this.calcCelsius(response.main.temp_max),
+				temp_min: this.calcCelsius(response.main.temp_min),
+				description: response.weather[0].description,
+				error: false
+			});
+			this.getWeatherIcon(this.weatherIcon, response.weather[0].id);
+
+	
+			this.getCovid();
+		} else {
+			this.setState({ error: true });
+		}
+		e[0].value = '';
+		e[1].value = '';
+	};
+	render() {
+		return (
+			<div className="App">
+				<Form loadWeather={this.getWeather} error={this.state.error} />
+				<Weather
+					city={this.state.city}
+					country={this.state.country}
+					temp_celsius={this.state.celsius}
+					temp_max={this.state.temp_max}
+					temp_min={this.state.temp_min}
+					description={this.state.description}
+					weathericon={this.state.icon}
+					covid={this.state.covid}
+					covidUpdateByCountry={this.state.covidUpdateByCountry}
+				/>
+			</div>
+		);
+	}
+}
+
+export default App;
